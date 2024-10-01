4107172117dfcb505dd4be2dbdaea48cafac4868
Arya-Kishan <arya12345kishan@gmail.com>
2024-10-01 21:58:16 +0530

    googleLoader

--- client/src/pages/Auth/LoginPage.jsx
+++ client/src/pages/Auth/LoginPage.jsx
@@ -15,6 +15,7 @@ import MyImage from '../../components/MyImage';
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from '../../services/Firebase';
 import useHandleError from '../../hooks/useHandleError';
+import Loader from '../../components/Loader';
 
 
 
@@ -25,6 +26,7 @@ const LoginPage = () => {
         formState: { errors },
     } = useForm();
     const [password, setPassword] = useState(false)
+    const [googleLoader, setGoogleLoader] = useState(false)
     const dispatch = useDispatch();
     const user = useSelector(selectLoggedInUser)
     const loginLoader = useSelector(selectLoginLoader)
@@ -51,93 +53,101 @@ const LoginPage = () => {
     const handleGoogleLogin = async () => {
 
         try {
-
             let newGoogleProvider = new GoogleAuthProvider();
+            setGoogleLoader(true);
             let result = await signInWithPopup(auth, newGoogleProvider);
             if (result.user) {
                 let { displayName, email, photoURL } = result.user;
                 dispatch(loginUserAsync({ email: email, password: null, loginThrough: 'google' }));
                 // SET THE USER DETAILS TO AUTH SLICE STATE WHICH GET FROM FIREBASE AND IT;S USED FOR ONLU SIGN OUT PURPOSE
                 dispatch(setGoogleUserDetails({ displayName, email, photoURL }))
+                setGoogleLoader(false);
             } else {
+                setGoogleLoader(false);
                 handleError("error", "error in login thorugh firebase google, google does not return the user details", "login page");
             }
 
         } catch (error) {
+            setGoogleLoader(false);
+            toast("try again");
             handleError(error, "error in login thorugh firebase google", "login page");
         }
 
     }
 
     return (
-        <div className='w-full h-dvh flex gap-5 justify-center items-center relative'>
+        googleLoader
+            ?
+            <Loader />
+            :
+            <div className='w-full h-dvh flex gap-5 justify-center items-center relative'>
 
-            <div className='w-[100%] md:w-[80%] h-[480px] shadow1 flex gap-2'>
-                {/* left */}
-                <div className='w-full md:w-[50%] h-full flex flex-col gap-10 justify-center items-center bg-bgBackground'>
+                <div className='w-[100%] md:w-[80%] h-[480px] shadow1 flex gap-2'>
+                    {/* left */}
+                    <div className='w-full md:w-[50%] h-full flex flex-col gap-10 justify-center items-center bg-bgBackground'>
 
-                    <div className='w-full flex justify-center items-center'>
-                        <MyImage src={logoIcon} className={"w-[90px] h-[90px]"} />
-                    </div>
-
-                    <form className='w-[80%] flex flex-col gap-6 text-center' onSubmit={handleSubmit(onSubmit)}>
-
-                        <div className='w-full bg-white rounded-lg px-2 flex items-center justify-start gap-2'>
-                            <MyImage src={personalIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-                            <input className='w-full p-2 rounded-lg' {...register('email', { required: true })} placeholder='Gmail...' />
+                        <div className='w-full flex justify-center items-center'>
+                            <MyImage src={logoIcon} className={"w-[90px] h-[90px]"} />
                         </div>
-                        {errors.email && <p className='text-red-600'>Email is required.</p>}
 
-                        <div className='w-full bg-white rounded-lg px-2 flex items-center justify-between gap-2'>
-                            <div className='w-full flex items-center'>
-                                <MyImage src={lockIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-                                <input className='w-full p-2 rounded-lg' type={password ? "text" : "password"} {...register('password', { required: true })} placeholder='Password...' />
+                        <form className='w-[80%] flex flex-col gap-6 text-center' onSubmit={handleSubmit(onSubmit)}>
+
+                            <div className='w-full bg-white rounded-lg px-2 flex items-center justify-start gap-2'>
+                                <MyImage src={personalIcon} alt="icon" className={"w-[30px] h-[30px]"} />
+                                <input className='w-full p-2 rounded-lg' {...register('email', { required: true })} placeholder='Gmail...' />
                             </div>
-                            <p onClick={() => setPassword(!password)} className='flex'>
-                                {password
-                                    ?
-                                    <MyImage src={openIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-                                    :
-                                    <MyImage src={closeIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-                                }
-                            </p>
+                            {errors.email && <p className='text-red-600'>Email is required.</p>}
+
+                            <div className='w-full bg-white rounded-lg px-2 flex items-center justify-between gap-2'>
+                                <div className='w-full flex items-center'>
+                                    <MyImage src={lockIcon} alt="icon" className={"w-[30px] h-[30px]"} />
+                                    <input className='w-full p-2 rounded-lg' type={password ? "text" : "password"} {...register('password', { required: true })} placeholder='Password...' />
+                                </div>
+                                <p onClick={() => setPassword(!password)} className='flex'>
+                                    {password
+                                        ?
+                                        <MyImage src={openIcon} alt="icon" className={"w-[20px] h-[20px]"} />
+                                        :
+                                        <MyImage src={closeIcon} alt="icon" className={"w-[20px] h-[20px]"} />
+                                    }
+                                </p>
+                            </div>
+                            {errors.password && <p className='text-red-600'>Password is required.</p>}
+
+                            <div className='text-bgBackground font-semibold relative'>
+                                <LoaderButton width={'100%'} text={"LOGIN"} bgColor='bg-[#75F94C]' loading={loginLoader} loaderColor={"#0A0A46"} />
+                                <p onClick={handleGuest} className='absolute top-full right-2 text-[12px] text-white pt-[2px]'>Login as Guest</p>
+                            </div>
+
+                        </form>
+
+                        <div className='w-[80%] flex justify-between items-center text-white'>
+                            <p className='w-[40%] h-[2px] bg-white'></p>
+                            <p>OR</p>
+                            <p className='w-[40%] h-[2px] bg-white'></p>
                         </div>
-                        {errors.password && <p className='text-red-600'>Password is required.</p>}
 
-                        <div className='text-bgBackground font-semibold relative'>
-                            <LoaderButton width={'100%'} text={"LOGIN"} bgColor='bg-[#75F94C]' loading={loginLoader} loaderColor={"#0A0A46"} />
-                            <p onClick={handleGuest} className='absolute top-full right-2 text-[12px] text-white pt-[2px]'>Login as Guest</p>
+                        <div onClick={handleGoogleLogin} className='w-[80%] p-2 flex justify-center gap-2 items-center text-center mt-2 rounded-lg border-2 border-white bg-bgBackground text-white' >
+                            <MyImage src={googleIcon} className={"w-[20px] h-[20px]"} />
+                            <span>Google</span>
                         </div>
 
-                    </form>
-
-                    <div className='w-[80%] flex justify-between items-center text-white'>
-                        <p className='w-[40%] h-[2px] bg-white'></p>
-                        <p>OR</p>
-                        <p className='w-[40%] h-[2px] bg-white'></p>
                     </div>
 
-                    <div onClick={handleGoogleLogin} className='w-[80%] p-2 flex justify-center gap-2 items-center text-center mt-2 rounded-lg border-2 border-white bg-bgBackground text-white' >
-                        <MyImage src={googleIcon} className={"w-[20px] h-[20px]"} />
-                        <span>Google</span>
+                    {/* right */}
+                    <div className='hidden md:flex w-[50%] h-full flex-col gap-5 justify-center items-center bg-[#1d1d71] text-white text-center'>
+                        <p className='text-2xl font-bold'>Welcome to WebBook</p>
+                        <p>Don't have an Account</p>
+                        <p onClick={() => navigate("/signup")} className='w-[100px] rounded-lg bg-bgBackground px-4 py-2'>Sign Up</p>
                     </div>
 
                 </div>
 
-                {/* right */}
-                <div className='hidden md:flex w-[50%] h-full flex-col gap-5 justify-center items-center bg-[#1d1d71] text-white text-center'>
-                    <p className='text-2xl font-bold'>Welcome to WebBook</p>
-                    <p>Don't have an Account</p>
-                    <p onClick={() => navigate("/signup")} className='w-[100px] rounded-lg bg-bgBackground px-4 py-2'>Sign Up</p>
+                <div className='block md:hidden w-full fixed bottom-2 left-0 text-center text-white text-[14px]'>
+                    <p onClick={() => navigate("/signup")} >Don't have account, <span className='text-customGreen'>Sign Up</span></p>
                 </div>
 
             </div>
-
-            <div className='block md:hidden w-full fixed bottom-2 left-0 text-center text-white text-[14px]'>
-                <p onClick={() => navigate("/signup")} >Don't have account, <span className='text-customGreen'>Sign Up</span></p>
-            </div>
-
-        </div>
     )
 }
 
--- client/src/pages/Auth/SignUpPage.jsx
+++ client/src/pages/Auth/SignUpPage.jsx
@@ -16,6 +16,7 @@ import { Navigate, useNavigate } from 'react-router-dom';
 import MyImage from '../../components/MyImage';
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from '../../services/Firebase';
+import Loader from '../../components/Loader';
 
 const SignUpPage = ({ handleToggleAuthPage }) => {
   const {
@@ -24,6 +25,7 @@ const SignUpPage = ({ handleToggleAuthPage }) => {
     formState: { errors },
   } = useForm();
   const [password, setPassword] = useState(false)
+  const [googleLoader, setGoogleLoader] = useState(false)
   const dispatch = useDispatch();
   const user = useSelector(selectLoggedInUser)
   const navigate = useNavigate();
@@ -55,6 +57,7 @@ const SignUpPage = ({ handleToggleAuthPage }) => {
 
     try {
       let newGoogleProvider = new GoogleAuthProvider();
+      setGoogleLoader(true);
       let result = await signInWithPopup(auth, newGoogleProvider);
       if (result.user) {
         let { displayName, email, photoURL } = result.user;
@@ -67,7 +70,9 @@ const SignUpPage = ({ handleToggleAuthPage }) => {
         dispatch(registerUserAsync(newUser));
         // SET THE USER DETAILS TO AUTH SLICE STATE WHICH GET FROM FIREBASE AND IT;S USED FOR ONLU SIGN OUT PURPOSE
         dispatch(setGoogleUserDetails({ displayName, email, photoURL }))
+        setGoogleLoader(false);
       } else {
+        setGoogleLoader(false);
         toast("try again")
         handleError("error", "error in sign up thorugh firebase google, google does not return the user details", "signup page");
       }
@@ -80,80 +85,84 @@ const SignUpPage = ({ handleToggleAuthPage }) => {
   }
 
   return (
-    <div className='w-full h-screen flex gap-5 justify-center items-center relative'>
+    googleLoader
+      ?
+      <Loader />
+      :
+      <div className='w-full h-screen flex gap-5 justify-center items-center relative'>
 
-      <div className='w-full md:w-[80%] h-[480px] shadow1 flex gap-2'>
-        {/* left */}
-        <div className='w-full md:w-[50%] h-full flex flex-col gap-8 justify-center items-center bg-bgBackground'>
+        <div className='w-full md:w-[80%] h-[480px] shadow1 flex gap-2 mb-[60px]'>
+          {/* left */}
+          <div className='w-full md:w-[50%] h-full flex flex-col gap-8 justify-center items-center bg-bgBackground'>
 
-          <div className='w-full flex justify-center items-center'>
-            <MyImage src={logoIcon} className={"w-[90px] h-[90px]"} alt={"logo"} />
-          </div>
-
-          <form className='w-[80%] flex flex-col gap-6 text-center' onSubmit={handleSubmit(onSubmit)}>
-
-            <div className='w-full bg-white rounded-lg px-2 flex items-center justify-start gap-2'>
-              <MyImage src={personalIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-              <input className='w-full p-2 rounded-lg' {...register('name')} placeholder='Name...' />
+            <div className='w-full flex justify-center items-center'>
+              <MyImage src={logoIcon} className={"w-[90px] h-[90px]"} alt={"logo"} />
             </div>
-            {errors.name && <p>Name is required.</p>}
 
-            <div className='w-full bg-white rounded-lg px-2 flex items-center justify-start gap-2'>
-              <MyImage src={emailIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-              <input className='w-full p-2 rounded-lg' {...register('email')} placeholder='Email...' />
-            </div>
-            {errors.email && <p>Email is required.</p>}
+            <form className='w-[80%] flex flex-col gap-6 text-center' onSubmit={handleSubmit(onSubmit)}>
 
-            <div className='w-full bg-white rounded-lg px-2 flex items-center justify-between gap-2'>
-              <div className='w-full flex items-center'>
-                <MyImage src={lockIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-                <input className='w-full p-2 rounded-lg' type={password ? "text" : "password"} {...register('password', { required: true })} placeholder='Password...' />
+              <div className='w-full bg-white rounded-lg px-2 flex items-center justify-start gap-2'>
+                <MyImage src={personalIcon} alt="icon" className={"w-[30px] h-[30px]"} />
+                <input className='w-full p-2 rounded-lg' {...register('name')} placeholder='Name...' />
               </div>
-              <p onClick={() => setPassword(!password)} className='flex'>
-                {password
-                  ?
-                  <MyImage src={openIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-                  :
-                  <MyImage src={closeIcon} alt="icon" className={"w-[30px] h-[30px]"} />
-                }
-              </p>
+              {errors.name && <p>Name is required.</p>}
+
+              <div className='w-full bg-white rounded-lg px-2 flex items-center justify-start gap-2'>
+                <MyImage src={emailIcon} alt="icon" className={"w-[30px] h-[30px]"} />
+                <input className='w-full p-2 rounded-lg' {...register('email')} placeholder='Email...' />
+              </div>
+              {errors.email && <p>Email is required.</p>}
+
+              <div className='w-full bg-white rounded-lg px-2 flex items-center justify-between gap-2'>
+                <div className='w-full flex items-center'>
+                  <MyImage src={lockIcon} alt="icon" className={"w-[30px] h-[30px]"} />
+                  <input className='w-full p-2 rounded-lg' type={password ? "text" : "password"} {...register('password', { required: true })} placeholder='Password...' />
+                </div>
+                <p onClick={() => setPassword(!password)} className='flex'>
+                  {password
+                    ?
+                    <MyImage src={openIcon} alt="icon" className={"w-[20px] h-[20px]"} />
+                    :
+                    <MyImage src={closeIcon} alt="icon" className={"w-[20px] h-[20px]"} />
+                  }
+                </p>
+              </div>
+              {errors.password && <p>Password is required.</p>}
+
+              <div className='text-bgBackground font-semibold relative'>
+                <LoaderButton width={'100%'} text={"SIGN UP"} loading={loginLoader} bgColor='bg-[#75F94C]' loaderColor={"#0A0A46"} />
+                <p onClick={handleGuest} className='absolute top-full right-2 text-[12px] text-white pt-[2px]'>View as Guest</p>
+              </div>
+
+            </form>
+
+            <div className='w-[80%] flex justify-between items-center text-white'>
+              <p className='w-[40%] h-[2px] bg-white'></p>
+              <p>OR</p>
+              <p className='w-[40%] h-[2px] bg-white'></p>
             </div>
-            {errors.password && <p>Password is required.</p>}
 
-            <div className='text-bgBackground font-semibold relative'>
-              <LoaderButton width={'100%'} text={"SIGN UP"} loading={loginLoader} bgColor='bg-[#75F94C]' loaderColor={"#0A0A46"} />
-              <p onClick={handleGuest} className='absolute top-full right-2 text-[12px] text-white pt-[2px]'>View as Guest</p>
+            <div onClick={handleGoogleSignUp} className='w-[80%] p-2 flex justify-center gap-2 items-center text-center mt-2 rounded-lg border-2 border-white bg-bgBackground text-white' >
+              <MyImage src={googleIcon} className={"w-[20px] h-[20px]"} />
+              <span>Google</span>
             </div>
 
-          </form>
-
-          <div className='w-[80%] flex justify-between items-center text-white'>
-            <p className='w-[40%] h-[2px] bg-white'></p>
-            <p>OR</p>
-            <p className='w-[40%] h-[2px] bg-white'></p>
           </div>
 
-          <div onClick={handleGoogleSignUp} className='w-[80%] p-2 flex justify-center gap-2 items-center text-center mt-2 rounded-lg border-2 border-white bg-bgBackground text-white' >
-            <MyImage src={googleIcon} className={"w-[20px] h-[20px]"} />
-            <span>Google</span>
+          {/* right */}
+          <div className='hidden md:flex w-[50%] h-full flex-col gap-5 justify-center items-center bg-[#1d1d71] text-white text-center'>
+            <p className='text-2xl font-bold'>Welcome to WebBook</p>
+            <p>Already have an Account</p>
+            <p onClick={handleGoogleSignUp} className=' w-[80%] p-2 text-center mt-2 rounded-lg text-white border-2 border-white' >Google SignUp</p>
+            <p onClick={() => navigate("/login")} className='w-[100px] rounded-lg bg-bgBackground px-4 py-2'>Login</p>
           </div>
-
         </div>
 
-        {/* right */}
-        <div className='hidden md:flex w-[50%] h-full flex-col gap-5 justify-center items-center bg-[#1d1d71] text-white text-center'>
-          <p className='text-2xl font-bold'>Welcome to WebBook</p>
-          <p>Already have an Account</p>
-          <p onClick={handleGoogleSignUp} className=' w-[80%] p-2 text-center mt-2 rounded-lg text-white border-2 border-white' >Google SignUp</p>
-          <p onClick={() => navigate("/login")} className='w-[100px] rounded-lg bg-bgBackground px-4 py-2'>Login</p>
+        <div className='block md:hidden w-full fixed bottom-2 left-0 text-center text-white text-[14px]'>
+          <p onClick={() => navigate("/login")} >Already have account, <span className='text-customGreen'>Login</span></p>
         </div>
-      </div>
 
-      <div className='block md:hidden w-full fixed bottom-2 left-0 text-center text-white text-[14px]'>
-        <p onClick={() => navigate("/login")} >Already have account, <span className='text-customGreen'>Login</span></p>
       </div>
-
-    </div>
 
   )
 }
