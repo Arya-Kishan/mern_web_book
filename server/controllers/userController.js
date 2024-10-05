bde7cd09ad00d24182f5a396f93158f0560cb77e
Arya-Kishan <arya12345kishan@gmail.com>
2024-10-05 19:58:19 +0530

    sanitise_user_response

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,10 +14,13 @@ export const createUser = AsyncHandler(async (req, res) => {
     const newUser = await user.save();
     let jwtToken = jwt.sign({ name: newUser.name, email: newUser.email, userId: newUser._id }, jwtSecret)
     res.set("x-webbook-jwt-routes", jwtToken);
-    res.status(200).json({ data: newUser, message: "Success" });
+    res.status(200).json({ data: sanitiseResponse(newUser), message: "Success" });
+
     await sendMail("arya12345kishan@gmail.com", "New User Join WebBook", `${newUser.name}`, getNewUserNotificationHtml(newUser.name, newUser.email))
+
     await sendMail(newUser.email, "Joined WebBook", `${newUser.name}`, signUpTemplate(newUser.name));
-}, "error in registering user")
+    
+}, "error in registering user or sending mail")
 
 export const loginUser = AsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
@@ -28,10 +31,10 @@ export const loginUser = AsyncHandler(async (req, res) => {
     res.set("x-webbook-jwt-routes", jwtToken);
 
     if (req.body.loginThrough == "google") {
-        return res.status(200).json({ data: user, message: "Success" });
+        return res.status(200).json({ data: sanitiseResponse(user), message: "Success" });
     } else {
         if (req.body.password == user.password) {
-            return res.status(200).json({ data: user, message: "Success" });
+            return res.status(200).json({ data: sanitiseResponse(user), message: "Success" });
         } else {
             return res.status(400).json({ data: null, message: "WRONG PASSWORD" });
         }
@@ -45,18 +48,24 @@ export const getAllUser = AsyncHandler(async (req, res) => {
 
 export const getSingleUser = AsyncHandler(async (req, res) => {
     const doc = await User.findById(req.params.userId);
-    res.status(200).json({ data: doc, message: "Success" });
+    res.status(200).json({ data: sanitiseResponse(doc), message: "Success" });
 }, "error in getting single user")
 
 export const updateUser = AsyncHandler(async (req, res) => {
     console.log(req.body);
     const doc = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(200).json({ data: doc, message: "Success" });
+    res.status(200).json({ data: sanitiseResponse(doc), message: "Success" });
 }, 'error in updating task')
 
 
 export const checkUser = AsyncHandler(async (req, res, next) => {
     let data = jwt.verify(req.headers?.['x-webbook-jwt-routes'], jwtSecret)
     const doc = await User.findById(data.userId);
-    res.status(200).json({ data: doc, message: "Success" });
+    res.status(200).json({ data: sanitiseResponse(doc), message: "Success" });
 }, "ERROR IN VERIFYING GUEST USER")
+
+
+const sanitiseResponse = (user) => {
+    let updatedUser = { _id: user._id, name: user.name, email: user.email, role: user.role, createdAt: user.createdAt, FCMtoken: { deviceToken: user.FCMtoken.deviceToken, pushPermission: user.FCMtoken.pushPermission } }
+    return updatedUser;
+}
