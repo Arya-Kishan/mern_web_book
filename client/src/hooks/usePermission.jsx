866438e029ab0fe5a8c72bf8681bf2cbb0277acb
Arya-Kishan <arya12345kishan@gmail.com>
2024-10-30 13:12:03 +0530

    notificatin_automatic

--- client/src/hooks/usePermission.jsx
+++ client/src/hooks/usePermission.jsx
@@ -24,21 +24,28 @@ const usePermission = () => {
 
         if ("Notification" in window != true) {
             console.log("Browser des not support push notification");
-            UpdateNotification(null, "notSupported");
+            if (loggedInUser?.FCMtoken?.pushPermission !== "notSupported") {
+                UpdateNotification(null, "notSupported");
+            }
             return null;
         }
 
         const browserPermission = await Notification.requestPermission();
+        console.log(browserPermission);
 
         if (browserPermission == "denied") {
-            UpdateNotification(null, "denied");
+            if (loggedInUser?.FCMtoken?.pushPermission !== "denied") {
+                console.log("updating notification as denied");
+                UpdateNotification(null, "denied");
+            }
             return null;
         }
 
         if (browserPermission == "granted") {
-            if (loggedInUser?.FCMtoken?.pushPermission == "consentNeeded") {
+            if (loggedInUser?.FCMtoken?.pushPermission !== "accepted") {
                 let allowance = await FCMTokenGeneration();
                 if (allowance.permission == "accepted") {
+                    console.log("updating notification as accepted");
                     UpdateNotification(allowance, "accepted");
                 }
             }
